d95829f7f273546747b8fb9b37f3b21996b4b894
Alex <therebeloper@gmail.com>
2019-03-02 10:54:45 +0200

    final

--- TheBetweenClosedRangeOperator/RootViewController.swift
+++ TheBetweenClosedRangeOperator/RootViewController.swift
@@ -13,6 +13,14 @@ class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let text = "Hello"
+        
+        if 2 ... 10 ~= text.count {
+//        if text.count >= 2 && text.count <= 10 {
+            print("Valid text")
+        } else {
+            print("Inalid text")
+        }
     }
 
 
